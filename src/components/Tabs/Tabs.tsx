d1168088fbdbad0a1578b530a7e1bc310bfde69a
gst <pavhanys@gmail.com>
2024-11-01 19:47:52 +0100

    feat: improve tabs accessibility

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -16,24 +16,32 @@ export const Tabs = ({ items }: ITabsProps) => {
   return (
     <>
       <div className="mb-10 flex justify-center">
-        <div className="border-custom-balck-100/20 flex gap-x-0.5 rounded-button border">
-          {Object.entries(items).map(([key, value]) => (
-            <button
+        <div className="border-custom-balck-100/20 flex gap-x-0.5 rounded-button border" role="tablist" aria-label="Product list tabs">
+          {Object.entries(items).map(([key, value]) => {
+            const isActive = activeTab === key;
+            return <button
               key={key}
               onClick={() => setActiveTab(key)}
               className={clsx("-m-px rounded-button px-10 py-2 text-base", {
                 "block bg-gradient-to-l from-custom-orange-400 to-custom-orange-500 font-bold text-white shadow-tab hover:bg-gradient-to-r":
-                  activeTab === key,
-                "hover:underline": activeTab !== key,
+                isActive,
+                "hover:underline": !isActive,
               })}
+              role="tab"
+              aria-selected={isActive}
+              aria-controls={`panel-${key}`}
+              id={`tab-${key}`}
+              tabIndex={isActive ? 0 : -1}
             >
               {value.label}
-            </button>
-          ))}
+            </button>;
+          })}
         </div>
       </div>
       <Bar text="Buy multiples original canine or human products" />
-      {items[activeTab].content}
+      <div id={`panel-${activeTab}`} role="tabpanel" tabIndex={0} aria-labelledby={`tab-${activeTab}`}>
+        {items[activeTab].content}
+      </div>
     </>
   );
 };
